cd0dc3759cf768c9b03c690102937a60caa88f32
SaraLMS <slm.santos@campus.fct.unl.pt>
2025-09-30 10:59:00 +0100

    implemented classification pipeline and added missing documentation and init files

---
+++ HAR/__init__.py
@@ -0,0 +1,3 @@
+from classifier import classify_human_activities
+
+__all__ = ['classify_human_activities']
---
+++ signal_processing/__init__.py
@@ -0,0 +1,3 @@
+from pre_process import apply_pre_processing_pipeline
+
+__all__ = ['apply_pre_processing_pipeline']
